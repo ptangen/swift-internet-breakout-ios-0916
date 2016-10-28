f7645c8e65d9b40e6223f13a200932ac79a939d3
Johann Kerr <johannkerr@gmail.com>
2016-10-27 23:30:57 -0400

    Initial Commit

---
+++ niftyFlatironWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  niftyFlatironWeather
+//
+//  Created by Johann Kerr on 10/27/16.
+//  Copyright © 2016 Johann Kerr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
